065d6fa6b403848384844a8e97a9808743293b9f
Alfred024 <21030761@itcelaya.edu.mx>
2022-06-09 17:39:36 -0500

    Creación de función descuento y botón de cupones a medias

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Demostración de las prácticas realizadas en el Curso Práctico de Javascript
 - Paso dos: Implementar fórmulas en Js
 - Paso tres: Crear funciones
 - Paso cuatro: Integrar Js con HTML
+
+## Taller #2: porcentaajes y descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar la fórmulas en JavaScript 
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
---
+++ Taller_#2/descuentos.js
@@ -0,0 +1,43 @@
+
+//Esta función está simplificada a comparación a la del curso, pero es IMPORTANTE recordar que se recomienda mantener a cada variable con una solo responsabilidad, por lo que probablemente hubiera sido mejor hacerlo como en el curso se muestra
+// function calcularPrecioConDescuento(precioOriginal,descuento){
+//     return precioOriginal - (precioOriginal*(descuento/100));
+// }
+
+function calcularPrecioConDescuento(precioOriginal,descuento,cupon){
+    return precioOriginal - (precioOriginal*((descuento+cupon)/100));
+}
+
+function cupponDescuento(precioConDescuento, descuentoCuppon){
+    return precioConDescuento - (precioConDescuento*(descuentoCuppon/100));
+}
+
+// function calcularDescuentoButton(){
+//     const inputPrecio = document.getElementById("inputPrice");
+//     const valuePrecio = inputPrecio.value;
+//     const inputDiscount = document.getElementById("inputDiscount");
+//     const valueDiscount = inputDiscount.value;
+//     const inputCuppon = document.getElementById("inputCuppon");
+//     const valueCuppon = inputCuppon.value;
+//     const resultado = calcularPrecioConDescuento(valuePrecio,valueDiscount);
+
+//     const resultadoCuppon = cupponDescuento(resultado, valueCuppon);
+
+//     const respuesta = document.getElementById("result");
+//     respuesta.innerText = "Precio con descuento y cupón: $"+resultadoCuppon;
+// }
+
+function calcularDescuentoButton(){
+    const inputPrecio = document.getElementById("inputPrice");
+    const valuePrecio = inputPrecio.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const valueDiscount = inputDiscount.value;
+    const inputCuppon = document.getElementById("inputCuppon");
+    const valueCuppon = inputCuppon.value;
+    const resultado = calcularPrecioConDescuento(valuePrecio,valueDiscount,valueCuppon);
+
+    const respuesta = document.getElementById("result");
+    respuesta.innerText = "Precio con descuento y cupón: $"+resultado;
+}
+
+
