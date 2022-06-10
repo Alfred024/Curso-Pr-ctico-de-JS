1d37bf552e42648e1871f3b5e37cb98dc2939051
Alfred024 <21030761@itcelaya.edu.mx>
2022-06-10 18:00:48 -0500

    Creación de varios métodos pa ordenar arrays

--- Taller_#3/mediana.js
+++ Taller_#3/mediana.js
@@ -5,6 +5,66 @@ const lista = [
     40000000
 ];
 
+const lista2 = [
+    4,5,10,1,3,9
+];
+
+
+
+function ordenarArraySort(lista){
+    return lista.sort((a,b) => a - b);
+}
+
+//Método hecho por mi
+function ordenarArray2(array){
+
+    let aux;
+    let i2;
+    for(let i=0; i<=array.length; i++){
+        i2=0;
+        for(let j=1; j<=array.length-1; j++){
+            if(array[i2]>array[j]){
+                aux = array[i2];
+                array[i2] == array[j];
+                array[j] == aux;
+            }i2++;
+        };
+    };
+}
+
+//Este método es como el que hice pero mejor 
+function ordenarArrayYutu(array){
+
+    let aux;
+    for(let i=0; i<array.length-1; i++){
+        for(let j=0; j<array.length-1; j++){
+            if(array[j]>array[j+1]){
+                aux = array[j];
+                array[j] == array[j+1];
+                array[j+1] == aux;
+            };
+        };
+    } 
+}
+
+ //Funciones de el maestro JuanDC
+// const mitadLista = parseInt(lista.length/2);
+
+// function esPar(numero){
+//     if(numero % 2 === 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// let mediana;
+// if(esPar(lista.length)){
+//     mediana = (lista[mitadLista-1]+lista[mitadLista])/2;
+// }else{
+//     mediana = lista[mitadLista];
+// }
+
 
 //Función hecha por mi
 function calcularMediana(lista){
